f6d8ad2d102076fe0cbbaa3bfcc2d8ff88f97526
eswilson <eswilson4@gmail.com>
2018-09-13 23:08:46 -0700

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // mySQL
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "chertanovo421",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "chertanovo421",
+    database: "burger_db"
+  });
+};
+
 
 // connection
 connection.connect(function(err) {
